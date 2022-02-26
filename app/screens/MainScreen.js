fcbeb4da44e77f115602e6a834c2e28c1cdc792d
Quang Huy <bui.quang.huy.2002@gmail.com>
2022-02-26 18:14:55 +0800

    Update MainScreen.js

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Button, Text, StyleSheet } from 'react-native';
+import { View, Button, Text, StyleSheet, TextInput, Alert } from 'react-native';
 
 const MainScreen = ({ navigation }) => {
   return (
@@ -7,10 +7,10 @@ const MainScreen = ({ navigation }) => {
       <Text style={{ marginTop: 20, marginBottom: 20 }}>
         1. Capture a full screenshot of the suspicious message
       </Text>
-      <Text>Quick links</Text>
+      <Text>Quick links:</Text>
       <View style={{}}></View>
       <Text style={{ marginBottom: 20 }}>
-        Autofill with your screenshot, or fill manually:{' '}
+        2. Autofill with your screenshot, or fill manually:{' '}
       </Text>
       <View style={{ marginBottom: 20 }}>
         <Button
@@ -18,7 +18,38 @@ const MainScreen = ({ navigation }) => {
           onPress={() => Alert.alert('Uploaded image')}
         />
       </View>
-      <View></View>
+      <Text>Phone number: </Text>
+      <TextInput
+        style={{
+          marginHorizontal: 10,
+          height: 40,
+          borderStyle: 'dotted',
+          borderWidth: 1,
+          borderRadius: 1,
+        }}
+      ></TextInput>
+      <Text style={{ marginTop: 20 }}>Pasted message</Text>
+      <TextInput
+        underlineColorAndroid='transparent'
+        multiline={true}
+        style={{
+          marginTop: 10,
+          marginHorizontal: 10,
+          height: 100,
+          borderStyle: 'dotted',
+          borderWidth: 1,
+          borderRadius: 1,
+          fontSize: 12,
+
+          textAlignVertical: 'top',
+        }}
+      ></TextInput>
+      <View style={{ marginTop: 20 }}>
+        <Button
+          title='Deep check by AI'
+          onPress={() => Alert.alert('Uploaded image')}
+        />
+      </View>
     </View>
   );
 };

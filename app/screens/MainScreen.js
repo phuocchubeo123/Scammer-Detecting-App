60f0a4b8573f446ae0991ae8d5a2a018ad07b215
Quang Huy <bui.quang.huy.2002@gmail.com>
2022-02-26 22:35:50 +0800

    Update MainScreen.js

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -7,8 +7,12 @@ import {
   TextInput,
   Alert,
   Image,
+  Linking,
+  TouchableOpacity,
 } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import OpenApplication from 'react-native-open-application';
+import { ScrollView } from 'react-native-gesture-handler';
 
 // const ReadText = require('text-from-image');
 const MainScreen = ({ navigation }) => {
@@ -36,12 +40,21 @@ const MainScreen = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.screen}>
+    <ScrollView style={styles.screen}>
       <View style={{ marginLeft: 15, marginRight: 15 }}>
-        <Text style={{ marginTop: 20, marginBottom: 10, color: '#474747' }}>
-          1. Capture a screenshot of the suspicious message
+        <Text
+          style={{
+            marginTop: 20,
+            marginBottom: 10,
+            color: '#474747',
+            fontWeight: 'bold',
+          }}
+        >
+          1. Capture a screenshot of the message
         </Text>
-        <Text style={{ color: '#474747', marginBottom: 10 }}>Quick links:</Text>
+        <Text style={{ color: '#474747', marginBottom: 10, marginLeft: 10 }}>
+          Quick links:
+        </Text>
         <View
           style={{
             flexDirection: 'row',
@@ -50,35 +63,87 @@ const MainScreen = ({ navigation }) => {
           }}
         >
           <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Image
-              style={{ width: 50, height: 50 }}
-              source={require('../assets/message.png')}
-            />
+            <TouchableOpacity onPress={() => Linking.openURL('sms:+123456789')}>
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={require('../assets/message.png')}
+              />
+            </TouchableOpacity>
+
             {/* <Text style={{ fontSize: 11, marginTop: 10 }}>Message</Text> */}
           </View>
           <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Image
-              style={{ width: 70, height: 70 }}
-              source={require('../assets/whatsapp.jpg')}
-            />
+            <TouchableOpacity
+              onPress={() =>
+                Linking.openURL(
+                  'whatsapp://send?phone=${whatsappNo}&text=${whatsappMsg}'
+                )
+              }
+            >
+              <Image
+                style={{ width: 70, height: 70 }}
+                source={require('../assets/whatsapp.jpg')}
+              />
+            </TouchableOpacity>
+
             {/* <Text style={{ fontSize: 11 }}>Whatsapp</Text> */}
           </View>
           <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Image
-              style={{ width: 50, height: 50 }}
-              source={require('../assets/telegram.jpg')}
-            />
+            <TouchableOpacity
+              onPress={() => {
+                Linking.openURL('https://t.me/');
+              }}
+            >
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={require('../assets/telegram.jpg')}
+              />
+            </TouchableOpacity>
+
             {/* <Text style={{ fontSize: 11 }}>Telegram</Text> */}
           </View>
         </View>
-        <Text style={{ marginBottom: 20, color: '#474747', marginTop: 20 }}>
+        <Text
+          style={{
+            marginBottom: 20,
+            color: '#474747',
+            marginTop: 20,
+            fontWeight: 'bold',
+          }}
+        >
           2. Autofill with your screenshot/fill manually:{' '}
         </Text>
 
-        <View style={{ marginBottom: 20 }}>
-          <Button title='Upload image' onPress={uploadImage} />
+        <View
+          style={{
+            marginBottom: 30,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              width: '50%',
+              backgroundColor: '#34568B',
+              borderRadius: 10,
+              height: 30,
+              justifyContent: 'center',
+            }}
+            onPress={uploadImage}
+          >
+            <Text
+              style={{
+                fontSize: 15,
+                color: 'white',
+                textAlign: 'center',
+                fontWeight: 'bold',
+              }}
+            >
+              Upload image
+            </Text>
+          </TouchableOpacity>
         </View>
-        <Text>Phone number: </Text>
+        <Text style={{ marginLeft: 10 }}>Phone number: </Text>
         <TextInput
           style={{
             marginHorizontal: 10,
@@ -86,9 +151,10 @@ const MainScreen = ({ navigation }) => {
             borderStyle: 'dotted',
             borderWidth: 1,
             borderRadius: 1,
+            marginTop: 5,
           }}
         ></TextInput>
-        <Text style={{ marginTop: 20 }}>Pasted message</Text>
+        <Text style={{ marginTop: 20, marginLeft: 10 }}>Pasted message: </Text>
         <TextInput
           underlineColorAndroid='transparent'
           multiline={true}
@@ -100,18 +166,33 @@ const MainScreen = ({ navigation }) => {
             borderWidth: 1,
             borderRadius: 1,
             fontSize: 12,
-
             textAlignVertical: 'top',
           }}
         ></TextInput>
-        <View style={{ marginTop: 20 }}>
-          <Button
-            title='Deep check by AI'
+        <View style={{ marginTop: 40 }}>
+          <TouchableOpacity
             onPress={() => Alert.alert('Uploaded image')}
-          />
+            style={{
+              backgroundColor: '#FF6F61',
+              height: 40,
+              justifyContent: 'center',
+              borderRadius: 20,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                color: 'white',
+                textAlign: 'center',
+                fontWeight: 'bold',
+              }}
+            >
+              Deep check by AI
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 

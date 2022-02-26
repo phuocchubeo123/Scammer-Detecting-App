16bf3f8ae6ea0a97c64c7174b9f4a7ec0156a511
phuoc <phuoc002@e.ntu.edu.sg>
2022-02-27 02:07:05 +0800

    server fix image bug

--- server/server.py
+++ server/server.py
@@ -1,3 +1,4 @@
+import io
 import pickle
 from flask import Flask, request
 from PIL import Image
@@ -23,8 +24,10 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 
 @app.route('/server/scanner/')
 def base64_scanner():
-    image_base64 = request.args.get("base64")
-    image_encode = base64.encodebytes(image_base64)
+    image_base64 = bytes(request.args.get("base64"), encodings = 'utf-8')
+    msg = base64.b64decode(image_base64)
+    buf = io.BytesIO(msg)
+    image_encode = Image.open(buf)
     return pytesseract.image_to_string(image_encode)
 
 if __name__ == "__main__":
